6d1df552e9f0c1827ef60514bfd3d081b3591adc
Karthikeyan Shanmugam <karthikeyan@bangthetable.com>
2021-12-23 12:58:03 +0530

    feat: add cloudinary integration

---
+++ src/api/cloudinary-test/controllers/cloudinary-test.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  cloudinary-test controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::cloudinary-test.cloudinary-test');
